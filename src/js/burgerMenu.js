a59916dedea7c4da58a4f6f9ceac6eb8a531b6a6
z9877969 <z9877969@ukr.net>
2023-06-07 19:28:30 +0300

    adds js to burger menu

---
+++ src/js/burgerMenu.js
@@ -0,0 +1,24 @@
+const burgerMenu = document.querySelector('.js-burger-menu');
+
+const toggleBurgerMenu = e => {
+  if (
+    !e.target.classList.contains('js-burger-toggle-btn') &&
+    !e.target.closest('button')?.classList.contains('js-burger-toggle-btn')
+  ) {
+    return;
+  }
+  burgerMenu.classList.toggle('is-open');
+  if (burgerMenu.classList.contains('is-open')) {
+    burgerMenu.addEventListener('click', closeBurgerClickByLink);
+  } else {
+    burgerMenu.removeEventListener('click', closeBurgerClickByLink);
+  }
+};
+
+const closeBurgerClickByLink = e => {
+  if (e.target.nodeName === 'a') return;
+  burgerMenu.classList.toggle('is-open');
+  burgerMenu.removeEventListener('click', closeBurgerClickByLink);
+};
+
+document.body.addEventListener('click', toggleBurgerMenu);
